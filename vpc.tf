aee4483aea44559e5613a33ca79adb69299a5e46
Sanjeev Vijapur <vijapursanjeev@yahoo.com>
2021-05-18 12:50:39 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network" {
+  source  = "app.terraform.io/sanjeevvijapur-training/network/google"
+  version = "2.5.0"
+  network_name = "sanjeevvijapur-network"
+  project_id = var.project
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+  # insert required variables here
+}
